4457b92e749d511324c7ec0c38cb416788353da7
Kendall Stephens <kendoll.stephens@gmail.com>
2020-06-25 21:25:28 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -10,8 +10,8 @@ def link(warning = "It's Dangerous To Go Alone! Take This.")
   puts warning
 end
 
-def all_phrases
-  mario(phrase)
-  toadstool(status)
-  link(warning)
+def all_phrases(mario, toadstool, link)
+  puts mario
+  puts toadstool
+  puts link
 end
