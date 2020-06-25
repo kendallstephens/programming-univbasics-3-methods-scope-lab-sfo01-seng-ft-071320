2cfffd9160c4416239af40709af45dd9f8492693
Kendall Stephens <kendoll.stephens@gmail.com>
2020-06-25 20:56:24 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,7 @@
-def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+def mario(phrase = "It's-a me, Mario!")
   puts phrase
 end
 
-def toadstool
+def toadstool(status = "Thank You Mario! But Our Princess Is In Another Castle!")
   puts status
 end
